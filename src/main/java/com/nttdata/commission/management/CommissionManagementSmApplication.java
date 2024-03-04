689f54b8d48803787f419ce15b15ae69cd8b1255
flex18 <emendozaminchan@gmail.com>
2024-03-04 11:25:24 -0500

    add structure-ms

---
+++ src/main/java/com/nttdata/commission/management/CommissionManagementSmApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.commission.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommissionManagementSmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommissionManagementSmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/commission/management/CommissionManagementSmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.commission.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommissionManagementSmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
